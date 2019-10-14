131ff76f660a6c3174d32e58963052de52240969
Andreas Hadiyono <andre.hadiyono@gmail.com>
2019-10-14 16:36:55 +0700

    first commit

---
+++ guestbook.php
@@ -0,0 +1,19 @@
+<?php
+echo "masuk";
+$nama=$_GET['nama'];
+$alamat=$_GET['alamat'];
+$tempat=$_GET['tempat'];
+$tgl_lahir=$_GET['tgl_lahir'];
+
+echo" Testing post
+nama = $nama;
+alamat = $alamat;
+tempat = $tempat;
+tgl_lahir = $tgl_lahir";
+
+//create connection dengan menggunakan mysql
+
+
+
+
+?>
